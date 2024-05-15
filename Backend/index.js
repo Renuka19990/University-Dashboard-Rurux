bf2aba33bfaf26ac0ce1393deb5d7acb30e7dbcb
renuka <renukasjagrwal@gmail.com>
2024-05-15 11:14:28 +0530

    some styling

--- Backend/index.js
+++ Backend/index.js
@@ -8,29 +8,30 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cors());
 
 // Allow specific origins
-const allowedOrigins = ['http://localhost:5173', 'https://university-dashboard-rurux.onrender.com'];
+// const allowedOrigins = ['http://localhost:5173', 'https://university-dashboard-rurux.onrender.com'];
 
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true,
-};
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//     if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   },
+//   credentials: true,
+// };
 
-app.use(cors(corsOptions));
+
 
 // Logging middleware to confirm origins
-app.use((req, res, next) => {
-  console.log('Request Origin:', req.headers.origin);
-  console.log('Request Headers:', req.headers);
-  next();
-});
+// app.use((req, res, next) => {
+//   console.log('Request Origin:', req.headers.origin);
+//   console.log('Request Headers:', req.headers);
+//   next();
+// });
 
 app.use('/studentApi', studentRouter);
 app.use('/adminApi',adminRouter);
